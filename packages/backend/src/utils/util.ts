2369de96be3e91da53b45f63f4d72f6cc01c6d38
Pure <111236541+PureStupid@users.noreply.github.com>
2024-05-26 04:01:02 +0800

    Create util.ts

---
+++ packages/backend/src/utils/util.ts
@@ -0,0 +1,22 @@
+/**
+ * Checks if a value is empty.
+ * @param value - The value to check.
+ * @returns Returns true if the value is empty, otherwise returns false.
+ */
+export const isEmpty = (value: string | number | object): boolean => {
+  if (value === null) {
+    return true;
+  } else if (typeof value !== "number" && value === "") {
+    return true;
+  } else if (typeof value === "undefined" || value === undefined) {
+    return true;
+  } else if (
+    value !== null &&
+    typeof value === "object" &&
+    !Object.keys(value).length
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+};
