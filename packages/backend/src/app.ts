e5031300be41c0c37fdf51d32e0315f81df20cbe
Pure <111236541+PureStupid@users.noreply.github.com>
2024-05-26 04:00:06 +0800

    Create app.ts

---
+++ packages/backend/src/app.ts
@@ -0,0 +1,77 @@
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import hpp from "hpp";
+import { connect, set, disconnect } from "mongoose";
+import { NODE_ENV, PORT, ORIGIN, CREDENTIALS } from "@config";
+import { dbConnection } from "@databases";
+import { Routes } from "@interfaces/routes.interface";
+import errorMiddleware from "@middlewares/error.middleware";
+
+class App {
+  public app: express.Application;
+  public env: string;
+  public port: string | number;
+
+  constructor(routes: Routes[]) {
+    this.app = express();
+    this.env = NODE_ENV || "development";
+    this.port = PORT || 3000;
+
+    this.connectToDatabase();
+    this.initializeMiddlewares();
+    this.initializeRoutes(routes);
+    this.initializeErrorHandling();
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.info(`=================================`);
+      console.info(`======= ENV: ${this.env} =======`);
+      console.info(`🚀 App listening on the port ${this.port}`);
+      console.info(`=================================`);
+    });
+  }
+
+  public async closeDatabaseConnection(): Promise<void> {
+    try {
+      await disconnect();
+      console.log("Disconnected from MongoDB");
+    } catch (error) {
+      console.error("Error closing database connection:", error);
+    }
+  }
+
+  public getServer() {
+    return this.app;
+  }
+
+  private async connectToDatabase() {
+    if (this.env !== "production") {
+      set("debug", true);
+    }
+
+    set("strictQuery", true);
+    await connect(dbConnection.url);
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(cors({ origin: ORIGIN, credentials: CREDENTIALS }));
+    this.app.use(hpp());
+    this.app.use(helmet());
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+  }
+
+  private initializeRoutes(routes: Routes[]) {
+    routes.forEach((route) => {
+      this.app.use("/", route.router);
+    });
+  }
+
+  private initializeErrorHandling() {
+    this.app.use(errorMiddleware);
+  }
+}
+
+export default App;
