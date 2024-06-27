8fa31ced5fe22fe281a88436ff1b6d170aac5fb7
Pure <111236541+PureStupid@users.noreply.github.com>
2024-06-27 14:24:27 +0800

    Update server.ts

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -3,9 +3,15 @@ import AuthRoute from "@routes/auth.route";
 import IndexRoute from "@routes/index.route";
 import UsersRoute from "@routes/users.route";
 import validateEnv from "@utils/validateEnv";
+import ProtectedRoute from "./routes/protected.route";
 
 validateEnv();
 
-const app = new App([new IndexRoute(), new UsersRoute(), new AuthRoute()]);
+const app = new App([
+  new IndexRoute(),
+  new UsersRoute(),
+  new ProtectedRoute(),
+  new AuthRoute(),
+]);
 
 app.listen();
