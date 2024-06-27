494bf4acbf61aa0a96f62623c49dfcbf9ec3ac6f
Pure <111236541+PureStupid@users.noreply.github.com>
2024-06-27 14:23:59 +0800

    Update auth.controller.ts

--- packages/backend/src/controllers/auth.controller.ts
+++ packages/backend/src/controllers/auth.controller.ts
@@ -1,29 +1,39 @@
 import { NextFunction, Request, Response } from "express";
 import { CreateUserDto } from "@dtos/users.dto";
 import { RequestWithUser } from "@interfaces/auth.interface";
-import { User } from "@interfaces/users.interface";
+import { User, UserRole } from "@interfaces/users.interface";
 import AuthService from "@services/auth.service";
 
 class AuthController {
   public authService = new AuthService();
 
-  public signUp = async (req: Request, res: Response, next: NextFunction) => {
+  public signUp = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    role: UserRole
+  ) => {
     try {
-      const userData: CreateUserDto = req.body;
-      const signUpUserData: User = await this.authService.signup(userData);
+      const userData: CreateUserDto = { ...req.body, role };
+      await this.authService.signup(userData);
 
-      res.status(201).json({ data: signUpUserData, message: "signup" });
+      res.status(201).json({ message: "signup success" });
     } catch (error) {
       next(error);
     }
   };
 
-  public logIn = async (req: Request, res: Response, next: NextFunction) => {
+  public logIn = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    role: UserRole
+  ) => {
     try {
-      const userData: CreateUserDto = req.body;
-      const { findUser } = await this.authService.login(userData);
+      const userData: CreateUserDto = { ...req.body, role };
+      await this.authService.login(userData);
 
-      res.status(200).json({ data: findUser, message: "login" });
+      res.status(200).json({ message: "login success" });
     } catch (error) {
       next(error);
     }
@@ -37,9 +47,9 @@ class AuthController {
   ) => {
     try {
       const userData: User = req.user;
-      const logOutUserData: User = await this.authService.logout(userData);
+      await this.authService.logout(userData);
 
-      res.status(200).json({ data: logOutUserData, message: "logout" });
+      res.status(200).json({ message: "logout success" });
     } catch (error) {
       next(error);
     }
